1c9242a169e39a2c1f86bbfbec2b5044b8aabc36
phxNguyen <nhuphuoc3008@hotmail.com>
2022-09-11 23:55:07 +0700

    add handle method argument not valid exception

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -2,16 +2,22 @@ package com.springboot.blog.exception;
 
 
 import com.springboot.blog.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
     // handle specific exceptions
@@ -35,5 +41,26 @@ public class GlobalExceptionHandler {
 
         ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // method lay trong docs ResponseEntityExceptionHandler
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+
+        // tao ra Map Object de chua <fieldName, validationMessage>
+        // ap dung cho all fields trong DTO
+        Map<String, String> errors = new HashMap<>();
+
+        // lay tat error tu exception object (MethodArgumentNotValidException ex)
+        // getAllErrors(): Get all errors, both global and field ones.
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName =((FieldError)error).getField();
+            String validationMessage = error.getDefaultMessage();
+            errors.put(fieldName,validationMessage);
+        });
+
+        return  new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        //return handleExceptionInternal(ex, null, headers, status, request);
     }
 }
